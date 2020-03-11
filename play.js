fabe9139e805097c9a730d0400337fbfa5d3ac7b
aliyasser20 <aliyasser20@hotmail.com>
2020-03-11 13:08:51 -0400

    added handleUserInput and setupInput

--- play.js
+++ play.js
@@ -1,3 +1,25 @@
 const {connect} = require("./client");
+
+const handleUserInput = (stdin) => {
+  stdin.on("data", (key) => {
+    // process.stdout.write(key);
+    if (key === "\u0003") {
+      process.exit();
+    }
+  });
+};
+
+const setupInput = () => {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  handleUserInput(stdin);
+  stdin.resume();
+  return stdin;
+};
+
+setupInput();
+
+
 console.log("Connecting ...");
 connect();
